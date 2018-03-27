53bb869972203c00f6498fa6b6b35e0a8d0bd0fb
Sunketo <sunketo1010@gmail.com>
2018-03-27 17:45:31 +0800

    Update Search_Insert_Position.cc

--- Search_Insert_Position.cc
+++ Search_Insert_Position.cc
@@ -13,7 +13,7 @@ public:
                 return middle;
             else if(nums[middle]>target)
             {
-                right=middle;
+                right=middle-1;
                 
             }if(nums[middle]<target)
             {
@@ -21,6 +21,27 @@ public:
             }
         }
         return low;*/
+        
+        
+       /*  int low = 0, high = nums.size()-1;
+
+        // Invariant: the desired index is between [low, high+1]
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+
+            if (nums[mid] < target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+         return low;*/
+        // (1) At this point, low > high. That is, low >= high+1
+        // (2) From the invariant, we know that the index is between [low, high+1], so low <= high+1. Follwing from (1), now we know low == high+1.
+        // (3) Following from (2), the index is between [low, high+1] = [low, low], which means that low is the desired index
+        //     Therefore, we return low as the answer. You can also return high+1 as the result, since low == high+1
+      
+        
+        
         if (nums.back() < target) return nums.size();
         int left = 0, right = nums.size() - 1;
         while (left < right) {
