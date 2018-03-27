b62ed1213485f1885acd13f9352b0fce5c8cb1a5
Sunketo <sunketo1010@gmail.com>
2018-03-27 15:20:33 +0800

    Update remove_dumplicate.cc
    对于一个数组中进行特定的覆盖之类的操作，根据两个指针（循环）进行操作，通过相反的条件进行循环来覆盖

--- remove_dumplicate.cc
+++ remove_dumplicate.cc
@@ -35,3 +35,21 @@ public:
         
     }
 };
+//移除指定的元素
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int len=nums.size();
+        if(len<=0)
+            return 0;
+        int j=0;
+        for(int i=0;i<len;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[j++]=nums[i];
+            }
+        }
+        return j;
+    }
+};
