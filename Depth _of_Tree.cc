92e4a657cd16474e78c0e84e50cd0ba7318a7633
Sunketo <sunketo1010@gmail.com>
2018-03-29 15:49:34 +0800

    Create Depth _of_Tree.cc

---
+++ Depth _of_Tree.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        int depth_l = maxDepth(root->left);
+        int depth_r = maxDepth(root->right);
+
+        return  > depth_r  ? depth_l + 1:depth_r + 1;
+        //这样写返回不了结果？？why？
+       // return maxDepth(root->left)>maxDepth(root->right)?(maxDepth(root->left)+1):(maxDepth(root->right)+1);
+        
+        /*if(root!=NULL)
+        {
+            return (maxDepth(root->left)+1)>(maxDepth(root->right)+1)?(maxDepth(root->left)+1):(maxDepth(root->right)+1);
+        }
+        return 0;*/
+        
+    }
+};
